38bafdb74e5ed22169ca07332f5d225dd56ea705
hamzashahab1610 <hamzashahab1610@gmail.com>
2021-02-28 16:16:16 +0500

    v2

---
+++ client/build/precache-manifest.b6d39c78f7905b499395c43f7c726ee6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02b3d75a726321d2a14422e447e9631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cda25e5ce8abc4c74259",
+    "url": "/static/css/2.69233f45.chunk.css"
+  },
+  {
+    "revision": "90715ee0b0a218fbae72",
+    "url": "/static/css/main.cb6bc270.chunk.css"
+  },
+  {
+    "revision": "cda25e5ce8abc4c74259",
+    "url": "/static/js/2.0df88c6f.chunk.js"
+  },
+  {
+    "revision": "d59fdd398320fe2db75c4564cf9d7456",
+    "url": "/static/js/2.0df88c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90715ee0b0a218fbae72",
+    "url": "/static/js/main.bf98786a.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
