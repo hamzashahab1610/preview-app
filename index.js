07a38ba3a7f90503ad65b72a2d7fea7849869ecd
hamzashahab1610 <hamzashahab1610@gmail.com>
2021-02-28 16:19:24 +0500

    v2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use(fileRoute);
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "build", "index.html"));
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3030, () => {
